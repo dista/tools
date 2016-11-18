0f6e21b16b8e3f47c864739be5439e79c1736f1c
dista <dista@qq.com>
2016-11-18 21:11:40 +0800

    add poision_dns_ips

--- ss-ev-scripts/start_ss.sh
+++ ss-ev-scripts/start_ss.sh
@@ -1,2 +1,5 @@
-sudo ss-redir -u -c ssserver.json -f /var/run/ssserver.pid
-sudo ss-nat -s 173.254.236.78 -l 12345 -b "211.103.128.226" -i chn.acl -u -o
+#!/bin/bash
+
+sudo ss-redir -u -c /home/dista/Documents/ssserver.json -f /var/run/ssserver.pid
+# wtf, 113.215.6.x wasu
+sudo ss-nat -s 173.254.236.78 -l 12345 -w "`cat /home/dista/Documents/poison_dns_ips.acl`" -i /home/dista/Documents/chn.acl -u -o
